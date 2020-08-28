58c7ba959c11a35a661d27bc09857c82116e0baa
Artem <ahaidai@lenovo.com>
2020-08-28 12:49:45 +0300

    03 Start Screen base components

--- SharedCode/src/commonMain/kotlin/domain/impl/ShuffleDeckInteractorImpl.kt
+++
@@ -1,8 +0,0 @@
-package domain.impl
-
-import data.model.Card
-
-class ShuffleDeckInteractorImpl: ShuffleDeckInteractor {
-
-    override fun shuffleDeck(orderedDeck: List<Card>) = orderedDeck.shuffled()
-}
